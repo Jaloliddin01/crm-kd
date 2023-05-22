0fe948357de5877f6ffa4f5d47beaac8edd9a9d8
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-05-23 02:28:48 +0500

    last change

--- kd/admin.py
+++ kd/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Staff, Child, Activity
 # Register your models here.
+
+admin.site.register(Staff)
+admin.site.register(Child)
+admin.site.register(Activity)
--- kd/models.py
+++ kd/models.py
@@ -3,10 +3,38 @@ from django.db import models
 # Create your models here.
 
 class Staff(models.Model):
+    UNVON = (
+        ('Direktor', 'Direktor'),
+        ("Katta o'qituvchi", "Katta o'qituvchi"),
+        ("Oshpaz", "Oshpaz"),
+        ("Qorovul", "Qorovul"),
+        ("Kichik o'qituvchi", "Kichik o'qituvchi")
+    )
+    FISH = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    birthplace = models.CharField(max_length=100)
+    millati = models.CharField(max_length=50)
+    unvoni = models.CharField(max_length=50, choices=UNVON)
+
+    def __str__(self):
+        return self.FISH
+
+class Child(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    birthplace = models.CharField(max_length=100)
+    millati = models.CharField(max_length=50)
+    yoshi = models.FloatField()
+    gruppasi = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+class Activity(models.Model):
+    name = models.CharField(max_length=50)
+    davomiyligi = models.FloatField()
+    teacher = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
